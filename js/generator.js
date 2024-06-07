7c80c3e1b6e414a9981415342476a78929f2d7ac
Nàt <82668500+4D4J@users.noreply.github.com>
2024-06-07 22:43:47 +0200

    Add files via upload

--- js/generator.js
+++ js/generator.js
@@ -1,38 +1,92 @@
-function BeteuSize() {
-    var input = document.getElementById("NAME").value.trim();
-    var resultParagraph = document.getElementById("RESULT");
-    var commentary = document.getElementById("COM");
+const nameDict = {};
+console.log(nameDict);
 
-    // Vérifiez si l'entrée n'est pas vide
-    if (input !== "") {
-        var randomNumber = (Math.random() * 100 + 1).toFixed();
-        resultParagraph.textContent = "the size of this name is : " + randomNumber + " cm";
-        switch (true) {
-            case randomNumber < 5:
-                commentary.textContent = "Bouhhhhh it's rikiki 😹😹😹";
-                break;
-            case randomNumber < 10:
-                commentary.textContent = "In french we say 'ptite zizi va ' 🤏";
-                break;
-            case randomNumber < 20:
-                commentary.textContent = "Okkk you're a boss brother 🙏🙏";
-                break;
-            case randomNumber < 30:
-                commentary.textContent = "Waw !! we owe you respect 💪💪💪💪";
-                break;
-            case randomNumber < 40:
-                commentary.textContent = "It's pretty good bro nice size 👍👍"
-            case randomNumber > 50:
-                commentary.textContent = "Sorry Sensei 😮";
-                break;
-            case randomNumber == 100:
-                commentary.textContent = "Hello penis master 🤯🤯";
-                break;
-            default:
-                commentary.textContent = "";
-                break;
-        }
+function showLoading() {
+    const generationDiv = document.querySelector('.generation');
+    const body = document.querySelector('body');
+    const txt = document.getElementById('TXT_TROLL');
+
+
+    generationDiv.style.display = 'flex';
+    body.style.opacity = '0.6';
+    body.style.zIndex = '3';
+    // text qui s'affiche au fur et à mesure du fake chargement
+    setTimeout(() => {
+        txt.textContent = 'Gender detection 🔎'
+    }, 1500);    
+    setTimeout(() => {
+        txt.textContent = 'Size determination 📐📐'
+    }, 3000);    
+    setTimeout(() => {
+        txt.textContent = 'Result ... 💡'
+    }, 5000);
+
+    setTimeout(() => {
+        generationDiv.style.display = 'none';
+        body.style.opacity = '1';
+        body.style.zIndex = '0';
+        BeteuSize();
+    }, 6500);
+}
+
+function BeteuSize() {
+    const input = document.getElementById("NAME").value.trim();
+    const resultParagraph = document.getElementById("RESULT");
+    const commentary = document.getElementById("COM");
+
+    if (!input) {
+        return;
+    }
+
+    let randomNumber;
+    if (nameDict.hasOwnProperty(input)) {
+        randomNumber = nameDict[input];
+        alert("deja dedans")
     } else {
-        commentary.textContent = ""; // Si l'entrée est vide, effacer le commentaire
+        randomNumber = (Math.random() * 100 + 1).toFixed();
+        nameDict[input] = randomNumber;
+    }
+    setTimeout(() => {
+        resultParagraph.textContent = `💥💥 The size of ${input} is: ${randomNumber} cm 💥💥`;
+    },500);
+    
+
+    switch (true) {
+        case randomNumber < 5:
+            commentary.textContent = "Bouhhhhh it's rikiki 😹😹😹";
+            break;
+        case randomNumber < 10:
+            commentary.textContent = "In French, we say 'ptite zizi va ' 🤏";
+            break;
+        case randomNumber < 20:
+            commentary.textContent = "Okkk you're a boss brother 🙏🙏";
+            break;
+        case randomNumber < 30:
+            commentary.textContent = "Waw !! we owe you respect 💪💪💪💪";
+            break;
+        case randomNumber < 40:
+            commentary.textContent = "It's pretty good bro nice size 👍👍";
+            break;
+        case randomNumber >= 50:
+            commentary.textContent = "Sorry Sensei 😮";
+            break;
+        case randomNumber === 100:
+            commentary.textContent = "Hello penis master 🤯🤯";
+            break;
+        default:
+            commentary.textContent = "";
+            break;
     }
 }
+
+document.querySelector('.bouton').addEventListener('click', () => {
+    const input = document.getElementById("NAME").value.trim();
+    if (input) {
+        if (nameDict.hasOwnProperty(input)) {
+            BeteuSize();
+        }else {
+            showLoading();
+        }
+    }
+    console.log(nameDict);
+});
